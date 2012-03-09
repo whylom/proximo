88d989c52928e57a8619508b79b4cf4464e7d101
David Stamm <dstamm@canvas.is>
2012-03-09 15:24:29 -0500

    Fixed bugs, updated code for newer Sinatra APIs, neatened comments.

--- proximo.rb
+++ proximo.rb
@@ -1,66 +1,33 @@
-# Proximo : a tiny proxy server based on Sinatra
-#
-# expects settings file (proximo.yml) in same directory, in this format:
-#
-#   local.domain.com:
-#     aliases:
-#       - local.alias.com
-#       - other.local.alias.com
-#     docroot: C:\local\static\files
-#     proxy: www.domain.com
-#     always_from_remote:
-#       - /filename.html
-#       - leading/slash/is-optional.css
-#       - /can/use/*/wildcards/*.jsp
-#
-# can also override remote host on a per-request basis
-#
-#   local.domain.com:
-#     docroot: C:\local\static\files
-#     proxy:
-#       default: www.domain.com
-#       others:
-#         - for: /javascripts/*
-#           use: localhost:3000
-
-require 'net/http'
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/reloader'
+require 'net/http'
 require 'yaml'
 require 'uri'
 
-
-
-# listen on default HTTP port, so simple hostnames can be used
+# Listen on default HTTP port and select config by the request's hostname.
 set :port, 80
 
-# do not automatically serve local files (all requests go through get/post handlers)
+# Do not automatically serve local files. We want all requests to go through 
+# routes, so we can dynamically determine what to do per request.
 set :static, false
 
-# this proxy gets seriously wacky running on Thin, so restrict server choices to Mongrel and WEBrick
-set :server, %w[mongrel webrick]
-
-
-
-# read from settings file before handling each request (no restarts required)
+# Read from settings file before handling each request (no restarts required).
 before do
   host = request.host
   @host = host
   @settings = settings_for(host)
   fail "Settings not defined for hostname '#{host}'." if @settings.nil?
 
-  # tell Sinatra where to find local static files
   docroot = @settings['docroot']
   fail "docroot not defined for hostname '#{host}'." if docroot.nil?
   fail "Docroot '#{docroot}' does not exist." if !File.exists?(docroot)
-  set :public, docroot
+  set :public_folder, docroot
 
-  # if the requested path ends in a slash, and an index.html is available in a local 
-  # folder with that name, explicitly append 'index.html' to the path
-  # otherwise, it is fetched from remote server (which routes to index.html, index.jsp, etc.)
+  # If the requested path ends in a slash, and an index.html is available in a
+  # local directory with that name, explicitly append 'index.html' to the path. 
+  # Otherwise, let the directory be fetched from remote server.
   path = request.path_info  
-  path << 'index.html' if ends_with_slash?(path) && exists_locally?(path + 'index.html')
+  path << 'index.html' if path.end_with?('/') && exists_locally?(path + 'index.html')
   @path = path
 
   # set remote_host based on proxy settings
@@ -112,11 +79,11 @@ def serve_local?
   # otherwise, serve local files if all of the following are true
   !matches_any?(@path, @settings['always_from_remote']) &&  # path does not match an 'always_from_remote' pattern
   exists_locally?(@path) &&                                 # path exists on local filesystem
-  !ends_with_slash?(@path)                                  # path is not a directory (forces fetch of remote 
+  !@path.end_with?('/')                                     # path is not a directory (forces fetch of remote 
 end                                                         #   directory indexes such as index.jsp)
 
 def serve_local
-  send_file(File.expand_path(options.public + unescape(@path)))
+  send_file(File.expand_path(settings.public_folder + unescape(@path)))
 end
 
 def serve_remote
@@ -146,10 +113,10 @@ end
 
 def custom_headers
   headers = {}  
-  headers['Cookie'] = @env['HTTP_COOKIE'] if !request.cookies.empty?
+  headers['Cookie']       = @env['HTTP_COOKIE'] if !request.cookies.empty?
   headers['Content-Type'] = request.content_type if request.post?
-  headers['User-Agent'] = request.user_agent
-  headers['Referer'] = @env['HTTP_REFERER'] if @env.has_key?('HTTP_REFERER')
+  headers['User-Agent']   = request.user_agent
+  headers['Referer']      = @env['HTTP_REFERER'] if @env.has_key?('HTTP_REFERER')
   headers
 end
 
@@ -177,16 +144,10 @@ def settings_for(hostname)
       end
     end
   end
-  
-  nil
-end
-
-def ends_with_slash?(path)
-  path =~ /\/$/
 end
 
 def exists_locally?(path)
-  File.exists?(File.join(options.public, path))
+  File.exists?(File.join(settings.public_folder, path))
 end
 
 def matches_any?(path, patterns)
@@ -194,7 +155,8 @@ def matches_any?(path, patterns)
   patterns.any? { |pattern| to_regexp(pattern) =~ path }
 end
 
-def to_regexp(string)  
+def to_regexp(string=nil)
+  return if string.nil? 
   # convert string pattern to regular expression
   # - replace * wildcard with regex wildcard
   # - surround pattern with ^ and $ anchors
